29954f0f64fc8285bdeb7934176ab129b93c6f08
Dad Sweater <132954889+dad-sweater@users.noreply.github.com>
2024-07-30 13:35:45 -0700

    Finished logic for determining the winner

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function getComputerChoice() {
   // Switch statement for the different random cases
   switch (Math.floor(Math.random() * 3)) {
     case 0:
-      computerChoice = "Rock!";
+      computerChoice = "rock";
       break;
     case 1:
-      computerChoice = "Paper!";
+      computerChoice = "paper";
       break;
     case 2:
-      computerChoice = "Scissors!";
+      computerChoice = "scissors";
   }
   return computerChoice;
 }
@@ -31,4 +31,29 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
   // Make the human choice case insensitive and trim any possible whitespace
   let lowerCaseChoice = humanChoice.trim().toLowerCase();
+
+  // Get message out for choices
+  console.log(`You chose ${lowerCaseChoice}!`);
+  console.log(`The computer chose ${computerChoice}`);
+
+  // Logic to determine the winner
+  if (lowerCaseChoice === "rock" && computerChoice === "rock") {
+    console.log(`You both made the same choice! It's a tie!`);
+  } else if (lowerCaseChoice === "rock" && computerChoice === "paper") {
+    console.log(`You lose! Paper beats rock!`);
+  } else if (lowerCaseChoice === "rock" && computerChoice === "scissors") {
+    console.log(`You win! Rock beats scissors!`);
+  } else if (lowerCaseChoice === "paper" && computerChoice === "rock") {
+    console.log(`You win! Paper beats rock!`);
+  } else if (lowerCaseChoice === "paper" && computerChoice === "paper") {
+    console.log(`You both made the same choice! It's a tie!`);
+  } else if (lowerCaseChoice === "paper" && computerChoice === "scissors") {
+    console.log(`You lose! Scissors beats papers!`);
+  } else if (lowerCaseChoice === "scissors" && computerChoice === "rock") {
+    console.log(`You lose! Rock beats scissors!`);
+  } else if (lowerCaseChoice === "scissors" && computerChoice === "paper") {
+    console.log(`You win! Scissors beats paper!`);
+  } else if (lowerCaseChoice === "scissors" && computerChoice === "scissors") {
+    console.log(`You both made the same choice! It's a tie!`);
+  }
 }
