7c1c93fb0bda2cd86ffe5d4df439dd14636882f1
dad-sweater <132954889+dad-sweater@users.noreply.github.com>
2024-09-24 09:37:03 -0700

    Assigned variables for the HTML score display and added code to disable the buttons once either player reaches 5 points

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 // Function to get the randomized computer choice
 function getComputerChoice() {
-  // Declare/initialize variable for the computer's choice
-  let computerChoice = "";
+    // Declare/initialize variable for the computer's choice
+    let computerChoice = "";
 
-  // Switch statement for the different random cases
-  switch (Math.floor(Math.random() * 3)) {
-    case 0:
-      computerChoice = "rock";
-      break;
-    case 1:
-      computerChoice = "paper";
-      break;
-    case 2:
-      computerChoice = "scissors";
-  }
-  return computerChoice;
+    // Switch statement for the different random cases
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+    }
+    return computerChoice;
+}
+
+// Function to evaluate if someone wins
+function determineWinner(humanScore, computerScore) {
+    if (humanScore === 5) {
+        console.log("You got 5 points! You win!");
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    } else if (computerScore === 5) {
+        console.log("The computer got 5 points! You lose!");
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    }
 }
 
 // Function to keep track of score and declare winner at 5 points
@@ -65,20 +80,25 @@ function playGame() {
         playRound("rock", getComputerChoice());
         console.log(computerScore);
         console.log(humanScore);
+        determineWinner(humanScore, computerScore);
     });
     paperBtn.addEventListener("click", () => {
         playRound("paper", getComputerChoice());
         console.log(computerScore);
         console.log(humanScore);
+        determineWinner(humanScore, computerScore);
     });
     scissorsBtn.addEventListener("click", () => {
         playRound("scissors", getComputerChoice());
         console.log(computerScore);
         console.log(humanScore);
+        determineWinner(humanScore, computerScore);
     });
-
-
 }
+
+// Declare human and computer scores for the DOM
+const humanScoreDisplay = document.querySelector("#humanScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
 
 // Declare buttons
 const rockBtn = document.querySelector('#rock')
